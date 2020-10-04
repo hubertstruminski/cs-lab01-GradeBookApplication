7ee6245fe68c19d655c19863c3a5712cc62d251f
Hubert <hubert.struminski@gmail.com>
2020-10-04 11:56:14 +0200

    add GreadBookType enum, property GradeBookType to BaseGradeBook class, add StandardGradeBook class, add RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            return base.GetLetterGrade(averageGrade);
+        }
+    }
+}
